5891c6d794fd6b1eaf9264af3370a47187f70640
goodnight_robicheaux <taeoo@hotmail.com>
2017-09-12 22:37:57 +0700

    Update output.js

--- src/component/output.js
+++ src/component/output.js
@@ -4,12 +4,8 @@ class output extends React.Component {
 	
 	constructor(){
     super();
-    
-    // console.log(this.props.match.params.value);
-    
   	}
     render(){
-    	// console.log(this.search.get("id"));
         const location = this.props.location;
     	const params = this.props.match.params;
         return (
